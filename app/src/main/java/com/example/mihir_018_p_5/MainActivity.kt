259e812bc5feb6c5b8820736caaa2486ccc051cd
bemihir001 <bemihir123@gmail.com>
2022-10-26 02:15:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mihir_018_p_5/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.mihir_018_p_5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun play(view: View) {
+        Intent(applicationContext, MyService::class.java)
+            .putExtra(MyService.DATA_KEY, MyService.DATA_VALUE)
+            .apply {
+                startService(this)
+            }
+        Toast.makeText(this, "button is clicked", Toast.LENGTH_LONG).show()
+    }
+
+    fun stop(view: View) {
+        Intent(applicationContext, MyService::class.java)
+            .apply {
+                stopService(this)
+            }
+        Toast.makeText(this, "button is clicked", Toast.LENGTH_LONG).show()
+    }
+
+}
